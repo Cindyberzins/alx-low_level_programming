fa8e71c2d8a971122143fd8282ad16ca0bebd8d5
cindyberzins <cindymwberzins@gmail.com>
2023-08-26 05:33:27 -0700

    trial2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, 59);
+
+	write(2, quote, 59); // 59 is the length of the quote including the null terminator
 	return (1);
 }
