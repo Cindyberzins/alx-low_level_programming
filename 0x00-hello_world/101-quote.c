5d167484b8973bf00389a7f7e29c7f3fed758154
cindyberzins <cindymwberzins@gmail.com>
2023-08-26 05:29:09 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point of the program
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, 59);
+	return (1);
+}
